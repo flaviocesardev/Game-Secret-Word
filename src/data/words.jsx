928675c768311fbc5d628e747d0c9f0d5e3063c0
flaviocesardev <flaviocesardev@gmail.com>
2024-01-04 17:56:32 -0300

    feat: adding new words

--- src/data/words.jsx
+++ src/data/words.jsx
@@ -1,8 +1,8 @@
 export const wordsList = {
-    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
-    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador"],
-    programação: ["Linguagem", "Framework", "JavaScript", "React", "Nodejs"],
-    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Banco", "Acelerador", "Buzina", "Embreagem", "Molas"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Melancia", "Abacate", "Abacaxi", "Amora", "Cajá"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Orelha", "Nariz", "Mão", "Dedos", "Dente"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador", "Fone", "Memória", "Cooler", "Fonte", "Cabos"],
+    programação: ["Linguagem", "Framework", "JavaScript", "ReactJs", "Nodejs", "Java", "Kotlin", "Express", "Redux", "Angular"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Pão", "Suco", "Macarrão", "Queijo", "Frango"],
 }
