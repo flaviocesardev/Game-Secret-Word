37d090a7795911b0935deff8aa20d8ec74f5eac4
Carlos Rogner <fviana.pitang@ons.org.br>
2024-05-08 22:01:42 -0300

    #Alinhamento visual StartScreen

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -4,7 +4,7 @@ const StartScreen = ({ startGame, }) => {
   return (
     <div className='start'>
         <h1>
-        <span>S</span>e<span>c</span>r<span>e</span><span>t</span> W<span>o</span>r<span>d</span>
+        <span>S</span>e<span>c</span>r<span>e</span>t W<span>o</span>r<span>d</span>
       </h1>
         {/* <p>Clique no botão abaixo para começar a jogar</p> */}
         <button onClick={startGame} className='buttonPlay'>Jogar!</button>
